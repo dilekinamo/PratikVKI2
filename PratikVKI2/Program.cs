9bcfcc90b03764186be3f4d30a5a347a64563eb9
dilekinamo <105610438+dilekinamo@users.noreply.github.com>
2022-08-17 01:55:07 +0200

    Add project files.

---
+++ PratikVKI2/Program.cs
@@ -0,0 +1,93 @@
+﻿public class Program { 
+    
+    struct Hasta    
+    {   
+        public string ad="";
+        public string soyad="";
+        public static double boy=1;
+        public static double kilo=0;
+        public static double vki = (kilo / (boy * boy)*10000);
+        public string teshis = Teshis(vki);
+
+       
+
+        public static string Teshis(double vki)
+        {
+            if (vki < 30 && vki >= 25)
+                return "HAFİF KİLOLU ->SARI";
+            else if (vki < 25 && vki >= 18.5)
+                return "NORMAL -> YEŞİL";
+            else if (vki >= 30)
+                return "OBEZ -> KIRMIZI";
+            else if (vki < 18.5)
+                return "ZAYIF -> SARI";
+            else
+                return "";
+        }
+        public void EkranaYaz()
+        {
+            Console.Write("Hasta: ");
+            Console.WriteLine($"{ad} {soyad}; boyu: {boy/100}, kilosu: {kilo}, VKI indeksi: {vki}, Teşhis: {teshis}");
+        }
+    }
+
+    public static void Main() {
+        Menu();
+    }
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("1. Yeni Hasta Hesabı\n2. Hasta Hesap Listesi\n3. Çıkış");
+        MenuSelection();
+    }
+
+    static void YeniHasta()
+    {
+        Hasta h;
+
+        Console.Write("Hasta Adı :");
+        h.ad = Console.ReadLine();
+        Console.Write("Hasta Soyadı :");
+        h.soyad = Console.ReadLine();
+        Console.Write("Hastanin Boyu (cm): ");
+        h.boy = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Hastanin Kilosu: ");
+        h.kilo = Convert.ToDouble(Console.ReadLine());
+        h.EkranaYaz();
+
+
+        //Hasta: Ahmet Bircan; boyu: 1.73, kilosu: 70, VKI indeksi: 25, Teşhis: Hafif Kilol
+        //Again();
+    }
+    static void MenuSelection()
+    {
+        Console.Write("Yapılacak işlemi seçin : ");
+        string choose = Console.ReadLine();
+        switch (choose)
+        {
+            case "1":
+                YeniHasta();
+                break;
+            case "2":
+                //HastalariListele();
+                break;
+            case "3":
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Hatalı seçim. Tekrar Deneyin.");
+                MenuSelection();//Recursive
+                break;
+
+        }
+    }
+}
+
+//Hasta: Ahmet Bircan; boyu: 1.73, kilosu: 70, VKI indeksi: 25, Teşhis: Hafif Kilolu
+
+//gelen hastaların ad, soyad, boy ve kilo bilgilerini alıp VKI ‘lerini hesaplamaları 
+
+//VKİ 18,4 ve altı- ZAYIF -> SARI
+//VKİ 18,5 - 24,9 arası NORMAL -> YEŞİL 
+//VKİ 25 - 29,9 arası ise HAFİF KİLOLU ->SARI
+//VKİ 30 ve fazlası OBEZ -> KIRMIZI
---
+++ Temp/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace Temp
+{
+    internal class Program
+    {
+        struct Hasta
+        {
+            public string ad;
+            public string soyad;
+            public double boy;
+            public double kilo;
+            
+            public double VKIHesapla()
+            {
+                return (kilo / (boy * boy) * 10000);
+            }
+
+            static string Teshis(double vki)
+            {
+                if (vki < 30 && vki >= 25)
+                    return "HAFİF KİLOLU ->SARI";
+                else if (vki < 25 && vki >= 18.5)
+                    return "NORMAL -> YEŞİL";
+                else if (vki >= 30)
+                    return "OBEZ -> KIRMIZI";
+                else if (vki < 18.5)
+                    return "ZAYIF -> SARI";
+                else
+                    return "";
+            }
+            public void EkranaYaz()
+            {
+                Console.Write("Hasta: ");
+                Console.WriteLine($"{ad} {soyad}; boyu: {boy}cm, kilosu: {kilo}kg, VKI indeksi: {Math.Round(VKIHesapla(),2)}, Teşhis: {Teshis(VKIHesapla())}");
+            }
+        }
+        static List <Hasta> HastaList = new List<Hasta>();
+        static void Main()
+        {
+            Console.Title = "VKI (vücut kitle indeksi) hesaplama";
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Yeni Hasta Hesabı\n2. Hasta Hesap Listesi\n3. Çıkış\n");
+            MenuSelection();
+        }
+        static void MenuSelection()
+        {
+            Console.Write("Yapılacak işlemi seçin : ");
+            string choose = Console.ReadLine();
+            switch (choose)
+            {
+                case "1":
+                    YeniHasta();
+                    break;
+                case "2":
+                    HastalariListele();
+                    break;
+                case "3":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Hatalı seçim. Tekrar Deneyin.");
+                    MenuSelection();//Recursive
+                    break;
+
+            }
+        }
+
+        static void HastalariListele()
+        {
+            Console.Clear();
+            foreach(var hasta in HastaList)
+            {
+                hasta.EkranaYaz();
+                Console.WriteLine("------------------------------------------------------------------------------------");
+            }
+            Console.WriteLine();
+            Tekrar();
+        }
+
+        public static void YeniHasta()
+        {
+            Hasta h;
+            Console.Write("\nHasta Adı :");
+            h.ad = Console.ReadLine();
+            Console.Write("Hasta Soyadı :");
+            h.soyad = Console.ReadLine();
+            Console.Write("Hastanin Boyu (cm): ");
+            h.boy = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Hastanin Kilosu: ");
+            h.kilo = Convert.ToDouble(Console.ReadLine());
+            HastaList.Add(h);
+            Console.WriteLine();
+            h.EkranaYaz();
+            Console.WriteLine();
+            Tekrar();
+        }
+
+        private static void Tekrar()
+        {
+            Console.WriteLine("Menüye dönmek için ENTER");
+            Console.ReadLine();
+            Menu();
+        }
+    }
+}
